37a5bc879e1a8b3dd71b5422c5767c402ce4fbc1
EduardLubomirsky <Lubomirsky.Eduard@gmail.com>
2018-07-06 19:12:27 +0300

    added page for login and replace button in modal box

--- app/js/common.js
+++ app/js/common.js
@@ -4,52 +4,47 @@ var loginForm = document.getElementsByClassName('fixed-overlay-login')[0];
 var mapContainer = document.getElementsByClassName('fixed-overlay-map')[0];
 var loginClose = document.getElementsByClassName('login-form-close-btn')[0];
 var mapClose = document.getElementsByClassName('map-close-btn')[0];
-btnOpenMap.onclick = function(event){
-    event.preventDefault();
-    if(mapContainer.classList.contains('open')) {
-      mapContainer.classList.remove('open');
-    }else{
-      mapContainer.classList.add('open');
-    }
+var loginField = document.querySelector('.login-form-item.login');
+try{
+	btnOpenMap.onclick = function(event){
+		event.preventDefault();
+		if(mapContainer.classList.contains('open')) {
+			mapContainer.classList.remove('open');
+		}else{
+			mapContainer.classList.add('open');
+		}
+	}
+}catch(ex){
+
 }
+try{
+	mapClose.onclick = function(){
+		mapContainer.classList.remove('open');
+	}
+}catch(ex){
 
-loginLink.onclick = function(event){
-    event.preventDefault();
-    if(loginForm.classList.contains('open')) {
-      loginForm.classList.remove('open');
-    }else{
-      loginForm.classList.add('open');
-    }
 }
-loginClose.onclick = function(){
-  loginForm.classList.remove('open');
+loginLink.onclick = function(evt){
+	evt.preventDefault();
+	loginForm.classList.add('open');
+	loginField.focus();
 }
-mapClose.onclick = function(){
-  mapContainer.classList.remove('open');
-}
-// .onclick = function(){
-//   loginForm.classList.remove('open');
-// }
-
-
-// btnOpenMap.addEventListener('click', function(e){
-//  alert("sosi huy")
-// });
-
-
-
-
+try{
+	loginClose.onclick = function(){
+		loginForm.classList.remove('open');
+	}
+}catch(ex){}
 
 
 function initMap() {
-  var uluru = {lat:59.938939, lng: 30.323105};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: uluru
-  });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
+	var uluru = {lat:59.938939, lng: 30.323105};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 15,
+		center: uluru
+	});
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+	});
 }
 
